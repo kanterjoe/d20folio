532771dfc7930f7503522c57621d315aca5125ae
Joe Kanter <kanterjoe@gmail.com>
2019-06-25 13:58:02 -0500

    Added Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 import logo from "./d20-folio-logo-1.png";
+import {Jumbotron} from 'reactstrap';
+import {BrowserRouter, Route} from 'react-router-dom'
+import NavigationBar from './components/Navbar';
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BrowserRouter>
+          <NavigationBar />
+          <Route exact path="/" component={() => <div>Hello WOrld</div>} />
+          <Route exact path="/mychars" component={() => <div>myChars</div>} />
+          <Route exact path="/new" component={() => <div>New</div>} />
+          <Route exact path="/edit/:id" component={({match}) => <div>Editing {match.params.id}</div>} />
+        </BrowserRouter>
       </div>
     );
   }
---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+export default class NavigationBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">
+              {/* <img className="img-fluid" src="/images/d20-folio-logo-1.png" /> */}
+              D20Folio</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+                <NavItem>
+                    <NavLink>
+                        <Link to='/'> Home </Link>
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>
+                        <Link to='/mychars'> My Characters </Link>
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>
+                        <Link to='/add'> Add New Character </Link>
+                    </NavLink>
+                </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
